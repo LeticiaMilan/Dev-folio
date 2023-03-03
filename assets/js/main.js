df1866d18b06d12a87d136e98da8d73b661c8125
LeticiaMilan <leticiamilan.ds@gmail.com>
2023-03-03 18:32:37 -0300

    add icons

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const hamburger_menu = document.querySelector(".hamburger-menu");
+
+function closeMenu() {
+    navbar.classList.remove("open");
+    document.body.classList.remove("stop-scrolling");
+}
+
+hamburger_menu.addEventListener("click", () => {
+    if (!navbar.classList.contains("open")) {
+        navbar.classList.add("open");
+        document.body.classList.add("stop-scrolling");
+    } else {
+        closeMenu();
+    }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
